638e42e65e0751913e93bf2d4789f387a49d3bdd
kodeking-081 <subash.procode@gmail.com>
2025-06-23 13:16:02 +0545

    Update login page

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -35,7 +35,7 @@ export async function POST(req: NextRequest) {
     value: token,
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'lax',
     path: '/',
     maxAge: 60 * 60 * 24, // 1 day
   });
